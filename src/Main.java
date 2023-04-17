cd9eab4b5a1cd6baa47fb23a7dbbd39febe7f116
Сергей <bondarevser.bondarev@yandex.ru>
2023-04-17 14:24:06 +0300

    (*) (/)

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,30 @@ public class Main {
                  dog = dog + 4;
                  System.out.println(dog);
                  cat = cat + 4;
-                 System.out.println(cat);
-                 paper = paper + 4;
-                 System.out.println(paper);
-                  dog = dog - 3.5;
-                  System.out.println(dog);
-                  cat = cat - 1.6;
-                  System.out.println(cat);
-                  paper = paper -7639;
-                  System.out.println(paper);
+                System.out.println(cat);
+                paper = paper + 4;
+                System.out.println(paper);
+                dog = dog - 3.5;
+                System.out.println(dog);
+                cat = cat - 1.6;
+                System.out.println(cat);
+                paper = paper - 7639;
+                System.out.println(paper);
 
-
+                var friend = 19;
+                System.out.println(friend);
+                friend = friend + 2;
+                System.out.println(friend);
+                friend = friend / 7;
+                System.out.println(friend);
+                var frog = 3.5;
+                System.out.println(frog);
+                frog = frog * 10;
+                System.out.println(frog);
+                frog = frog / 3.5;
+                System.out.println(frog);
+                frog = frog + 4;
+                System.out.println(frog);
             }
 
     }
