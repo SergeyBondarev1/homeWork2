a1af9ba69b77a280f8761bafc136434541ee93b1
Сергей <bondarevser.bondarev@yandex.ru>
2023-04-17 14:05:20 +0300

    var = dog +4

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,7 @@ public class Main {
                 System.out.println(cat);
                 paper = paper + 4;
                 System.out.println(paper);
-                dog = dog - 3.5;
-                System.out.println(dog);
-                cat = cat - 1.6;
-                System.out.println(cat);
-                paper = paper - 7639;
-                System.out.println(paper);
+
 
             }
 
